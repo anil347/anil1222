7dd203c17d4b070b51eef1c8f68aef2fdd09f6df
anil347 <120251905+anil347@users.noreply.github.com>
2022-12-10 16:46:21 +0530

    Add files via upload

---
+++ Exceptionresponse.java
@@ -0,0 +1,20 @@
+package com.ssd.empmanagement.Exceptions;
+
+public class Exceptionresponse {
+
+	
+	private String errormsg;
+
+	public String getErrormsg() {
+		return errormsg;
+	}
+
+	public void setErrormsg(String errormsg) {
+		this.errormsg = errormsg;
+	}
+
+	public Exceptionresponse(String string) {
+		super();
+	}
+	
+}
