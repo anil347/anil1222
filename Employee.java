2883cad6b8c98ff014d4fe7d7b8a6daf43320e2f
anil347 <120251905+anil347@users.noreply.github.com>
2022-12-10 16:29:23 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,86 @@
+package com.ssd.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "emp")
+public class Employee {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer empno;
+	private String ename;
+	private String job;
+	private Integer mgr;
+	private String hiredate;
+	private int sal;
+	private Double  comm;
+	private Integer deptno;
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Integer getMgr() {
+		return mgr;
+	}
+	public void setMgr(Integer mgr) {
+		this.mgr = mgr;
+	}
+	public String getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public Double getComm() {
+		return comm;
+	}
+	public void setComm(Double comm) {
+		this.comm = comm;
+	}
+	public Integer getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Integer deptno) {
+		this.deptno = deptno;
+	}
+	public Employee(Integer empno, String ename, String job, Integer mgr, String hiredate, int sal, Double comm,
+			Integer deptno) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.mgr = mgr;
+		this.hiredate = hiredate;
+		this.sal = sal;
+		this.comm = comm;
+		this.deptno = deptno;
+	}
+	public Employee() {
+		super();
+	}
+	
+}
